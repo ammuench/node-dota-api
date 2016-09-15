56f50f1407be66e7ebffb473edc952be6f2355fa
Alex Muench <ammuench@gmail.com>
2016-09-15 00:28:38 -0500

    Update to opendota since yasp is being phased out.  Add gameLength and gameType to matches objects.  Add daysSinceLastMatch value.  Expand status codes. Add total games played value to winLoss object.  Lock version in package.json

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 API to fetch Dota stats for players, heroes, matches, and more!
 
 ## What's New? ##
+    
+    **v1.0.0**
+    -Major stable release!
+    -Ported to use opendota instead of yasp
+    -Added in game length and game type
+    -Expanded status codes to be more descriptive
+    -Added 'daysSinceLastMatch' value
+    -Added total games played to 'winLoss' object
+    -Locked dependency versions for stability
+
+    ----
+
     v0.2.4
     -Added 'profileImage' to playerStats object
 
@@ -43,49 +55,60 @@ npm install node-dota-api
 Takes a PlayerID value as a string or integer, passes back a playerInfo JSON object through the callback with the following format:
 ```json
 {
-    status: "",
-    soloMMR: "",
-    partyMMR: "",
-    estMMR: "",
-    name: "",
-    winLoss: {
-        wins: "",
-        losses: "",
-        winrate: ""
+    "status": "",
+    "daysSinceLastMatch": "",
+    "soloMMR": "",
+    "partyMMR": "",
+    "estMMR": "",
+    "name": "",
+    "winLoss": {
+        "wins": "",
+        "losses": "",
+        "winrate": "",
+        "totalGames": ""
     },
-    mostPlayed: [
+    "mostPlayed": [
 		{
-            hero: "",
-            games: "",
-            winrate: ""
+            "hero": "",
+            "games": "",
+            "winrate": ""
+        },
+        {
+            "hero": "",
+            "games": "",
+            "winrate": ""
+        },
+        {
+            "hero": "",
+            "games": "",
+            "winrate": ""
         }
     ],
-    recentGames: [
+    "recentGames": [
 		{
-		    matchID: "",
-		    hero: "",
-		    outcome: "",
-		    whenPlayed: "",
-		    skillLevel: "",
-		    kda: ""
+		    "matchID": "",
+		    "hero": "",
+		    "outcome": "",
+            "gameMode": "",
+		    "whenPlayed": "",
+            "gameLength": "",
+		    "skillLevel": "",
+		    "kda": ""
 		}
     ],
-    profileURL: "",
-    profileImage: ""
+    "profileURL": "",
+    "profileImage": ""
 };
 ```
+###### Status Reports
 
-If there an invalid user is passed in, status will return as "Invalid User".  If it is a server error, status will return as "Error".
+If the user is valid, but their profile shows less than 20 games, the status will return as "**Partial Match History**".  This is likely due to match history not being shown, or an extremely new account
 
+If the user is valid, but their profile's last parsed game is over 14 days ago, the status will return as "**Outdated Match History**"
 
-## What's Planned ##
------
-Right now the api is being built out as we use it for some other projects, but some current future features include:
+If there an invalid user is passed in, status will return as "**Invalid User**". 
 
-* Looking up additional Player Info, like player trends or activity
-* Looking up hero and item info
-* Looking up Dota Team info
-
+If it is a server error, status will return as "**Error**".
 
 ## License ##
 -----
@@ -97,11 +120,13 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
+Users of this software will give the developer at least one ('1') high-five upon meeting him in person as a sign of gratitude.
+
 THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ## Special Thanks ##
 
-Shoutout to the guys at [Yasp.co](http://yasp.co), they do great work, and they're the reason this API works.  Go donate some cheese to help keep their systems running!
+Shoutout to the guys at [OpenDota](http://opendota.com), they do great work, and they're the reason this API works.  Go [donate and help carry](https://www.opendota.com/carry) them to keep their systems running!
 
 Many thanks to Carney for answer myriads of questions when I'm having issues
 
--- dotaPlayerApi.js
+++ dotaPlayerApi.js
@@ -16,37 +16,41 @@ module.exports = {
 
 	/**
 	 * [playerStats description]
-	 * @param  {string|integer}	playerID						Player's Dota ID Number
-	 * @param  {Function}		callback						Provides playerInfoJson object after running request call and logic
-	 * @property {object}		playerInfoJson					JSON object containing player stats and info
-	 * @property {string}		playerInfoJson.status			Status of request call.  Returns either 'Success' or 'Error'
-	 * @property {string}		playerInfoJson.soloMMR			Solo MMR Value
-	 * @property {string}		playerInfoJson.partyMMR			Party MMR Value
-	 * @property {string}		playerInfoJson.estMMR			Yasp Estimated MMR Value
-	 * @property {string}		playerInfoJson.name				User's current Profile Name
-	 * @property {object}		playerInfoJson.winLoss			Object of Win-Loss values
-	 * @property {string}		playerInfoJson.winLoss.wins		Number of wins (returned as string)
-	 * @property {string}		playerInfoJson.winLoss.losses	Number of losses (returned as string)
-	 * @property {string}		playerInfoJson.winLoss.winrate	Winrate percentage
-	 * @property {array}		playerInfoJson.mostPlayed		Array of User's 5 most played heroes, with games played and winrate
-	 * @property {array}		playerInfoJson.recentGames		Array of User's 20 most recent games, with matchID, hero, result, time played, and skill level
-	 * @property {string}		playerInfoJson.profileUrl		URL to user's proflie on Yasp.co
+	 * @param  {string|integer}	playerID							Player's Dota ID Number
+	 * @param  {Function}		callback							Provides playerInfoJson object after running request call and logic
+	 * @property {object}		playerInfoJson						JSON object containing player stats and info
+	 * @property {string}		playerInfoJson.status				Status of request call.  See status reports section in README.md
+	 * @property {float}		playerInfoJson.daysSinceLastMatch	Number of days since last match was played && parsed
+	 * @property {string}		playerInfoJson.soloMMR				Solo MMR Value
+	 * @property {string}		playerInfoJson.partyMMR				Party MMR Value
+	 * @property {string}		playerInfoJson.estMMR				OpenDota Estimated MMR Value
+	 * @property {string}		playerInfoJson.name					User's current Profile Name
+	 * @property {object}		playerInfoJson.winLoss				Object of Win-Loss values
+	 * @property {string}		playerInfoJson.winLoss.wins			Number of wins (returned as string)
+	 * @property {string}		playerInfoJson.winLoss.losses		Number of losses (returned as string)
+	 * @property {string}		playerInfoJson.winLoss.winrate		Winrate percentage
+	 * @property {string}		playerInfoJson.winLoss.totalGames	Number of total games played & parsed
+	 * @property {array}		playerInfoJson.mostPlayed			Array of User's 5 most played heroes, with games played and winrate
+	 * @property {array}		playerInfoJson.recentGames			Array of User's 20 most recent games, with matchID, hero, result, time played, and skill level
+	 * @property {string}		playerInfoJson.profileUrl			URL to user's proflie on OpenDota.com
 	 */
 	playerStats: function(playerID, callback) {
 
-		var profileUrl = 'https://yasp.co/players/' + playerID;
+		var profileUrl = 'https://opendota.com/players/' + playerID;
 
 
 		var playerInfoJson = {
-			status: "",
-			soloMMR: "",
-			partyMMR: "",
-			estMMR: "",
-			name: "",
+			status: '',
+			daysSinceLastMatch: '',
+			soloMMR: '',
+			partyMMR: '',
+			estMMR: '',
+			name: '',
 			winLoss: {
-				wins: "",
-				losses: "",
-				winrate: ""
+				wins: '',
+				losses: '',
+				winrate: '',
+				totalGames: ''
 			},
 			mostPlayed: [],
 			recentGames: [],
@@ -58,17 +62,17 @@ module.exports = {
 
 				var $ = cheerio.load(html);
 
-				playerInfoJson.status = "Success";
+				playerInfoJson.status = 'Success';
 
 				//Finds User's Profile Name
 				$('title').filter(function() {
-					playerInfoJson.name = $(this).text().split(' - YASP')[0];
+					playerInfoJson.name = $(this).text();
 				});
 
 				//If invalid values are passed in, the username will equal the invalid value, or NaN
 				if (playerInfoJson.name == playerID || playerInfoJson.name == 'NaN') {
 
-					playerInfoJson.status = "Invalid User";
+					playerInfoJson.status = 'Invalid User';
 
 				} else {
 
@@ -110,6 +114,7 @@ module.exports = {
 						//Assign array values to their correct properties
 						playerInfoJson.winLoss.wins = sourceInfoArray[0];
 						playerInfoJson.winLoss.losses = sourceInfoArray[2];
+						playerInfoJson.winLoss.totalGames = parseInt(sourceInfoArray[0]) + parseInt(sourceInfoArray[2]);
 						playerInfoJson.winLoss.winrate = sourceInfoArray[3].split('(')[1].split(')')[0];
 
 					});
@@ -125,7 +130,7 @@ module.exports = {
 						 * Iterates through heros from table and adds them to heroesArray
 						 * @param  	{Number}	limit	Defines how many heroes to add in array.  Max is 20			
 						 */
-						for (var i = 0, limit = 5; i < limit; i++) {
+						for (var i = 0, limit = 20; i < limit; i++) {
 
 							//Set individual hero row
 							var hero = heroes.eq(i).children('td');
@@ -168,7 +173,28 @@ module.exports = {
 							//Set individual hero row
 							var match = matches.eq(i).children('td');
 
-							// whenPlayedFormatted = moment.unix(match.eq(4).text());
+							/**
+							 * Parses gametime in seconds and returns formatted value
+							 * @param  {string} source 	Gametime in total seconds
+							 * @return {string} 		Game length in format HH:MM
+							 */
+							var parseGameTime = function(source){
+						 		var secondsTotal = source;
+						 		var min = parseInt(secondsTotal)/60;
+						 		var sec = '0.' + min.toString().split('.')[1];
+						 		sec = parseFloat(sec);
+						 		sec = Math.floor(sec * 60);
+
+						 		//add a leading '0' to single digit second values
+						 		if(sec < 10){
+						 			sec = '0' + sec;
+						 			
+						 		}
+
+						 		min = min.toString().split('.')[0]
+
+						 		return min + ':' + sec
+						 	}
 
 							/**
 							 * Object generated with match stats
@@ -182,9 +208,11 @@ module.exports = {
 							 */
 							var matchObj = {
 								matchID: match.eq(0).text(),
-								hero: match.eq(1).children('.img-sm').attr('title'),
+								hero: match.eq(1).children('.img-text').children('.img-sm').attr('title'),
 								outcome: match.eq(2).text(),
-								whenPlayed: moment.unix(match.eq(4).text()).format("MMM Do, YYYY"),
+								gameMode: match.eq(3).text(),
+								whenPlayed: moment.unix(match.eq(4).text()).format('MMM Do, YYYY @ hh:mma'),
+								gameLength: parseGameTime(match.eq(5).html()),
 								skillLevel: match.eq(6).text(),
 								kda: match.eq(7).text() + '/' + match.eq(8).text() + '/' + match.eq(9).text()
 							}
@@ -195,6 +223,18 @@ module.exports = {
 
 						//Set array in userInfoObject
 						playerInfoJson.recentGames = matchesArray;
+
+						if(matchesArray.length < 20){
+							playerInfoJson.status = 'Partial Match History';
+						}
+
+						var now = moment(new Date());
+						var lastGame = moment(matchesArray[0].whenPlayed, 'MMM Do, YYYY @ hh:mma');
+						var duration = moment.duration(now.diff(lastGame)).asDays();
+						playerInfoJson.daysSinceLastMatch = duration;
+						if(duration > 14){
+							playerInfoJson.status = 'Outdated Match History'
+						}
 
 					});
 
